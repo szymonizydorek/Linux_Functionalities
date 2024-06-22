0b8af9a3e863a08bb8126340b3600cbbc0115c38
szymonizydorek <izydorek.szymon@gmail.com>
2024-06-22 14:31:17 +0200

    WIP Changing Structre of Project

--- MAIN_SOFTWARE/Networking/Networking_Functions.sh
+++ MAIN_SOFTWARE/Networking/Networking_Functions.sh
@@ -1,8 +1,35 @@
 #!/bin/bash/
 echo "hello from Networking"
 
-function 1_Ping() {
+function Function_Ping1() {
   echo -e "This is day 1 of Bash Challenge \n Provide website address: "
   read website
-  ping -c 3 $website | grep loss
+  OUTPUT=$(ping -c 3 $website | grep loss)
+  echo $OUTPUT
 }
+
+function Function_tcpdum2(){
+
+  VERSION=$(sudo command tcpdump --version | awk 'NR==2 {print $3}')
+  if [[ $VERSION != 0 ]]; then
+    echo 'TCPDUMP is installed'
+  else
+    echo 'TCPDUMP is not installed'
+  fi
+
+  if [[ $VERSION != 0 ]]; then
+    echo 'TCPDUMP is installed'
+  else
+    echo 'TCPDUMP is not installed'
+  fi
+  
+  echo "AVAILABLE INTERFACES: "
+  printf "\n"
+  ifconfig | grep -i UP | awk '{print $1}' | sed 's/:/ /g' | grep '^en'
+  read -p "ENTER INTERFACE NAME: " INTERFACE
+  
+  echo "STARTING PACKET CAPTURE ON INTERFACE: $INTERFACE"
+  echo "OUTPUT FILE IS SAVED TO: /tmp/file2.txt"
+  sudo tcpdump -i $INTERFACE -c 10 > /tmp/file2.txt
+  echo "DONE"
+} 
